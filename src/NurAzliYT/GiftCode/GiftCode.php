a7f943d8037eeda9ea0a516eff6b530ae98fea12
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-11 18:32:34 +0700

    Add files via upload

--- src/NurAzliYT/GiftCode/GiftCode.php
+++ src/NurAzliYT/GiftCode/GiftCode.php
@@ -1,1 +1,34 @@
+<?php
 
+declare(strict_types=1);
+
+namespace NurAzliYT\GiftCode;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use NurAzliYT\GiftCode\commands\NhapCodeCommands;
+use NurAzliYT\GiftCode\commands\TaoCodeCommands;
+
+class GiftCode extends PluginBase {
+
+	public static $instance;
+
+	public static function getInstance() : self {
+		return self::$instance;
+	}
+
+	public function onEnable() : void {
+		$this->registerCommands();
+		$this->code = new Config($this->getDataFolder() . "codes.yml", Config::YAML);
+		self::$instance = $this;
+	}
+
+	private function registerCommands() : void {
+		$this->getServer()->getCommandMap()->register("/klaimkode", new NhapCodeCommands($this));
+		$this->getServer()->getCommandMap()->register("/buatkode", new TaoCodeCommands($this));
+	}
+
+	public function onDisable() : void {
+		$this->code->save();
+	}
+}
