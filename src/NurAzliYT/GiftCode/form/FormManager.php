4c101adbc0a925cef23da76d10a92dc765b16d59
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-07 16:03:52 +0700

    Add files via upload

--- src/NurAzliYT/GiftCode/form/FormManager.php
+++ src/NurAzliYT/GiftCode/form/FormManager.php
@@ -1,16 +1,17 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types=1); // BỐ LƯỜI CLEAN OK :))))
 
-namespace NurAzliYT\GiftCode\form;
+namespace NoobMCBG\GiftCode\form;
 
 use pocketmine\player\Player;
 use jojoe77777\FormAPI\CustomForm;
-use cooldogepmdev\BedrockEconomy\BedrockEconomy;
+use onebone\economyapi\EconomyAPI;
 use onebone\coinapi\CoinAPI;
-use NurAzliYT\GiftCode\GiftCode;
+use NoobMCBG\GiftCode\GiftCode;
 
 class FormManager {
+
 	private GiftCode $plugin;
 
 	public function __construct(GiftCode $plugin){
@@ -27,28 +28,28 @@ class FormManager {
 			// Số Tiền Tặng ( Ghi 0 nếu Ko Tặng Tiền ) = $data[3]
 			// Giống 3 Nhưng Đổi Thành Coin = $data[4]
 			if(!isset($data[1])){
-				$player->sendMessage("§l§cPlease enter the code you want to create in the first box");
+				$player->sendMessage("§l§cSilakan Masukkan Kode yang Ingin Anda Buat Di Kotak Pertama");
 				return true;
 			}
 			if(!isset($data[2])){
-				$player->sendMessage("§l§cPlease enter the number of people who can enter this code in the 2nd box");
+				$player->sendMessage("§l§cSilakan masukkan jumlah orang yang dapat memasukkan kode ini di kotak ke-2");
 				return true;
 			}
 			if(!isset($data[3])){
-				$player->sendMessage("§bPlease enter the amount of Money you want to give in this gift code (Enter 0 if not giving Money)");
+				$player->sendMessage("§bSilakan masukkan jumlah Uang yang ingin Anda berikan dalam kode hadiah ini (Masukkan 0 jika tidak memberikan Uang)");
 				return true;
 			}
 			if(!isset($data[4])){
-				$player->sendMessage("§bPlease enter the number of Coins you want to give away in this gift code (Enter 0 if not giving Coins)");
+				$player->sendMessage("§bSilakan masukkan jumlah Koin yang ingin Anda berikan dalam kode hadiah ini (Masukkan 0 jika tidak memberikan Koin)");
 				return true;
 			}
 			if(!is_numeric($data[2]) or !is_numeric($data[3]) or !is_numeric($data[4])){
-				$player->sendMessage("§bThe second box that must be entered is a number");
+				$player->sendMessage("§bKotak nomor 2 yang harus dimasukkan adalah nomor");
 				return true;
 			}
 			if($this->plugin->code->exists($data[1])){
 				$count = $this->plugin->code->get($data[1])["count"];
-				$player->sendMessage("§cThe $data[1] code already exists and has $count of entries remaining");
+				$player->sendMessage("§cKode $data[1] sudah ada dan memiliki $count entri yang tersisa");
 				return true;
 			}
 			$this->plugin->code->set($data[1], [
@@ -58,10 +59,16 @@ class FormManager {
 				"coin" => (int)$data[4]
 			]);
 			$this->plugin->code->save();
-			$player->sendMessage("§bSuccessfully create Giftcode $data[1] with input $data[2], the prize is $data[3] Coin and $data[4] Coin
+			$player->sendMessage("§bBerhasil membuat Giftcode $data[1] dengan input $data[2], hadiahnya adalah $data[3] Coin dan $data[4] Coin
 ");
 		});
-		#This
+		$form->setTitle("Buat Kode Hadiah");
+		$form->addLabel("Silakan isi apa yang perlu Anda isi di bawah ini");
+		$form->addInput("Kode:", "Mis: QNLYYO7588");
+		$form->addInput("Jumlah orang yang bisa masuk:", "Contoh: 5");
+		$form->addInput("Jumlah yang diberikan:", "Contoh: 1 (Tulis 0 jika tidak memberikan Uang)");
+		$form->addInput("Jumlah koin yang disumbangkan:", "Contoh: 2 (Tulis 0 jika Anda tidak menyumbangkan Koin)");
+		$form->sendToPlayer($player);
 	}
 
 	public function menuEnterCode(Player $player){
@@ -71,11 +78,11 @@ class FormManager {
 				return true;
 			}
 			if(!$this->plugin->code->exists($data[0])){
-				$player->sendMessage("§cThis Code Doesn't Exist!");
+				$player->sendMessage("§cKode Ini Tidak Ada!");
 				return true;
 			}
 			if((int)$this->plugin->code->get($data[0])["count"] < 1){
-				$player->sendMessage("§cThis Code Has Expired!!");
+				$player->sendMessage("§cKode Ini Telah Kedaluwarsa!");
 				return true;
 			}
 			$ex = explode(", ", $this->plugin->code->get($data[0])["enter-by-player-list"]);
@@ -86,18 +93,18 @@ class FormManager {
 				$this->plugin->code->save();
 				$money = (int)$this->plugin->code->get($data[0])["money"];
 				$coin = (int)$this->plugin->code->get($data[0])["coin"];
-				$player->sendMessage("§bEnter the code successfully and you have received $money Money and $coin Coin");
-                                BedrockEconomy::getInstance()->addMoney($player, $money);
+				$player->sendMessage("§bMasukkan kode dengan sukses dan Anda telah menerima $money Uang dan $coin Coin");
+				EconomyAPI::getInstance()->addMoney($player, $money);
 				CoinAPI::getInstance()->addCoin($player, $coin);
 				$count = (int)$this->plugin->code->get($data[0])["count"];
 				$this->plugin->code->setNested($data[0] . ".count", $count - 1);
 				$this->plugin->code->save();
 			}else{
-				$player->sendMessage("§cYou have entered this code before!");
+				$player->sendMessage("§cAnda telah memasukkan kode ini sebelumnya!");
 			}
 		});
-		$form->setTitle("Enter Code");
-		$form->addInput("Enter the code you want to enter below:", "Example: BD148SF8GJQ");
+		$form->setTitle("Masukkan Code");
+		$form->addInput("Masukkan kode yang ingin Anda masukkan di bawah ini:", "Contoh: BD148SF8GJQ");
 		$form->sendToPlayer($player);
 	}
 }
