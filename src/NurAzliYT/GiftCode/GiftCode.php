b895184d52d33a13d8b7ab1f166a1134128ad107
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-10 13:28:56 +0700

    Update GiftCode.php

--- src/NurAzliYT/GiftCode/GiftCode.php
+++ src/NurAzliYT/GiftCode/GiftCode.php
@@ -7,18 +7,17 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use jojoe77777\FormAPI\CustomForm;
-use cooldogedev\BedrockEconomy\BedrockEconomy; // Import namespace BedrockEconomy
+use cooldogedev\BedrockEconomy\BedrockEconomy;
 
 class GiftCode extends PluginBase {
 
     private BedrockEconomy $economy;
 
-        // Inisialisasi BedrockEconomy
-    public function onEnable():void {
+    public function onEnable(): void {
         $this->economy = $this->getServer()->getPluginManager()->getPlugin(BedrockEconomy::class);
 
         if ($this->economy === null) {
-            $this->getLogger()->error("BedrockEconomy plugin not found. Make sure it's installed.");
+            $this->getLogger()->error("Plugin BedrockEconomy not found. Please make sure it's installed.");
             $this->getServer()->getPluginManager()->disablePlugin($this);
         }
     }
@@ -28,10 +27,10 @@ class GiftCode extends PluginBase {
             if ($sender instanceof Player) {
                 $this->openCodeForm($sender);
             } else {
-                $sender->sendMessage("§cPerintah ini hanya dapat digunakan oleh pemain.");
+                $sender->sendMessage("§cThis command can only be used by players.");
             }
         }
-        return false;
+        return true;
     }
 
     public function openCodeForm(Player $player) {
@@ -39,19 +38,17 @@ class GiftCode extends PluginBase {
             if ($data === null) {
                 return;
             }
-            $formType = (int)$data[0]; // Mendapatkan jenis formulir yang dipilih oleh pemain
+            $formType = (int)$data[0];
 
             if ($formType === 0) {
-                // Pilihan "Buat Kode" dipilih
                 $this->openCreateCodeForm($player);
             } elseif ($formType === 1) {
-                // Pilihan "Kirim Kode" dipilih
                 $this->openSendCodeForm($player);
             }
         });
 
-        $form->setTitle("Form Kode");
-        $form->addDropdown("Pilih jenis formulir:", ["Buat Kode", "Kirim Kode"]); // Menampilkan pilihan "Buat Kode" dan "Kirim Kode"
+        $form->setTitle("Code Form");
+        $form->addDropdown("Select form type:", ["Create Code", "Send Code"]);
         $player->sendForm($form);
     }
 
@@ -60,22 +57,21 @@ class GiftCode extends PluginBase {
             if ($data === null) {
                 return;
             }
-            $code = $data[0]; // Mendapatkan kode yang dimasukkan oleh pemain dari formulir
-            $numOfPlayers = (int)$data[1]; // Mendapatkan jumlah pemain yang dimasukkan oleh pemain dari formulir
-            $moneyAmount = (int)$data[2]; // Mendapatkan jumlah uang yang dimasukkan oleh pemain dari formulir
-            $coinAmount = (int)$data[3]; // Mendapatkan jumlah koin yang dimasukkan oleh pemain dari formulir
+            $code = $data[0];
+            $numOfPlayers = (int)$data[1];
+            $moneyAmount = (int)$data[2];
+            $coinAmount = (int)$data[3];
 
-            // Di sini, Anda dapat mengimplementasikan logika pembuatan kode sesuai dengan kebutuhan Anda
+            // Implement your code creation logic here
 
-            // Memberikan pemain pesan konfirmasi
-            $player->sendMessage("§aKode '$code' telah dibuat dengan input: Pemain: $numOfPlayers, Uang: $moneyAmount, Koin: $coinAmount");
+            $player->sendMessage("§aCode '$code' has been created with input: Players: $numOfPlayers, Money: $moneyAmount, Coins: $coinAmount");
         });
 
-        $form->setTitle("Buat Kode");
-        $form->addInput("Kode", "Contoh: ABC123");
-        $form->addInput("Jumlah Pemain yang Dapat Memasukkan Kode", "Contoh: 5");
-        $form->addInput("Jumlah Uang dalam Kode", "Contoh: 100 (Tulis 0 jika tidak ada uang)");
-        $form->addInput("Jumlah Koin dalam Kode", "Contoh: 50 (Tulis 0 jika tidak ada koin)");
+        $form->setTitle("Create Code");
+        $form->addInput("Code", "Example: ABC123");
+        $form->addInput("Number of Players Who Can Use the Code", "Example: 5");
+        $form->addInput("Amount of Money in the Code", "Example: 100 (Enter 0 if no money)");
+        $form->addInput("Amount of Coins in the Code", "Example: 50 (Enter 0 if no coins)");
         $player->sendForm($form);
     }
 
@@ -88,35 +84,30 @@ class GiftCode extends PluginBase {
             $recipient = $data[1];
             $message = $data[2];
 
-            // Di sini Anda dapat mengimplementasikan logika pengiriman kode ke penerima
-            // serta mengupdate saldo pemain menggunakan BedrockEconomy
             if ($this->economy !== null) {
                 $this->sendMoneyUsingBedrockEconomy($player, $recipient, $codeToSend);
             } else {
-                $player->sendMessage("§cPlugin BedrockEconomy tidak ditemukan. Hubungi admin server.");
+                $player->sendMessage("§cBedrockEconomy plugin not found. Please contact the server admin.");
             }
 
-            // Memberikan pemain pesan konfirmasi
-            $player->sendMessage("§aKode '$codeToSend' telah dikirim ke '$recipient' dengan pesan: '$message'");
+            $player->sendMessage("§aCode '$codeToSend' has been sent to '$recipient' with message: '$message'");
         });
 
-        $form->setTitle("Kirim Kode");
-        $form->addInput("Kode", "Contoh: ABC123");
-        $form->addInput("Penerima", "Nama pemain yang akan menerima kode");
-        $form->addInput("Pesan", "Pesan tambahan (opsional)");
+        $form->setTitle("Send Code");
+        $form->addInput("Code", "Example: ABC123");
+        $form->addInput("Recipient", "Name of the player who will receive the code");
+        $form->addInput("Message", "Additional message (optional)");
         $player->sendForm($form);
     }
 
-    // Metode untuk mengirim uang menggunakan BedrockEconomy
     private function sendMoneyUsingBedrockEconomy(Player $sender, $recipient, $amount) {
-        // Pastikan penerima ada di server sebelum mengirim uang
         $recipientPlayer = $this->getServer()->getPlayerExact($recipient);
 
         if ($recipientPlayer instanceof Player) {
             $this->economy->addMoney($recipientPlayer, $amount);
-            $sender->sendMessage("§aAnda telah mengirim $amount mata uang kepada $recipient.");
+            $sender->sendMessage("§aYou have sent $amount currency to $recipient.");
         } else {
-            $sender->sendMessage("§cPenerima tidak ditemukan atau sedang offline.");
+            $sender->sendMessage("§cRecipient not found or currently offline.");
         }
     }
 }
